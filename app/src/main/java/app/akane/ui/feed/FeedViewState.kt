91005e65207c73997769f6ad39f30a946a9e8c55
amrro <amr.elghobary@gmail.com>
2019-04-08 21:59:19 +0200

    I missed committing some drawable files.

--- app/src/main/java/app/akane/ui/feed/FeedViewState.kt
+++ app/src/main/java/app/akane/ui/feed/FeedViewState.kt
@@ -6,7 +6,10 @@ import app.akane.data.entity.Post
 import com.airbnb.mvrx.MvRxState
 
 data class FeedViewState(
+//    val subredditName: String,
     val isLoading: Boolean = false,
     val feed: LiveData<PagedList<Post>>? = null,
     val isEmpty: Boolean = false
+//    val sorting: SubredditSort = SubredditSort.HOT,
+//    val timePeriod: TimePeriod? = null
 ) : MvRxState
